d6049087a4326f72f3a3304ef2a7c7631f4d1867
BadryansahBangsawan <badryansah99@gmail.com>
2026-05-10 13:26:09 +0800

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,13 @@
+import { env } from "@kabut-tipis/env/server";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+
+import * as schema from "./schema";
+
+export function createDb() {
+  const client = createClient({
+    url: env.DATABASE_URL,
+  });
+
+  return drizzle({ client, schema });
+}
